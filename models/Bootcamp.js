2c65287a3d4207eecae9a761ce30ba10287403c6
Moonyoung Chu <cnansdud11@hanmail.net>
2023-03-02 00:02:19 +0900

    Mongoose Middleware 사용해보기

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -108,5 +109,12 @@ const BootcampSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+
+//Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  console.log("Slugify ran", this.name);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
