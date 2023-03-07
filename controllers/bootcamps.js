27f64df8db85515bf5d9b517e255256a4c21370d
Moonyoung Chu <cnansdud11@hanmail.net>
2023-03-07 21:39:00 +0900

    feat: 반경내 부트캠프 찾는 api 추가

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const Bootcamp = require("../models/Bootcamp");
+const geocoder = require("../utils/geocoder");
 
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
@@ -61,4 +62,31 @@ exports.deleteBootcamps = asyncHandler(async (req, res, next) => {
     );
   }
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc        Get bootcamps within a radius
+// @route       GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @accees      Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get la/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius = 3, 963mi / 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,8 +5,11 @@ const {
   createBootcamps,
   updateBootcamps,
   deleteBootcamps,
+  getBootcampsInRadius,
 } = require("../controllers/bootcamps");
 const router = express.Router();
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
 router.route("/").get(getBootcamps).post(createBootcamps);
 
