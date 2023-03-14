9432c8ffc9ff76e84d13b39f737be542ee4a6140
Moonyoung Chu <cnansdud11@hanmail.net>
2023-03-15 00:16:16 +0900

    feat: User 추가 & password 암호화
    https://www.npmjs.com/package/bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,5 +3,17 @@ const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  console.log(">>>req", req.body)
+
+  //Create User
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
   res.status(200).json({ success: true });
 });
