6d547420d93c62ff1a2edb1d0c1aeb980c8875df
Moonyoung Chu <cnansdud11@hanmail.net>
2023-05-17 15:38:30 +0900

    feat: course model 추가 & seeder에 course 추가

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({path:'./config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 const User = require('./models/User');
 
 // Connect to DB
@@ -18,6 +19,10 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
@@ -26,6 +31,7 @@ const users = JSON.parse(
 const importData = async () => {
     try {
       await Bootcamp.create(bootcamps);
+      await Course.create(courses);
       await User.create(users);
       console.log('Data Imported...'.green.inverse);
       process.exit();
@@ -38,6 +44,7 @@ const importData = async () => {
   const deleteData = async () => {
     try {
       await Bootcamp.deleteMany();
+      await Course.deleteMany();
       await User.deleteMany();
       console.log('Data Destroyed...'.red.inverse);
       process.exit();
